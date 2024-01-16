38236b94d252377f83b10783f95295514ce1e2a1
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2024-01-16 02:33:24 +0100

    Update `lint-posts.sh`

--- lint-posts.sh
+++ lint-posts.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Lint the posts against stylistic errors.
+#
 # author: andreasl
 
 # Check for wrong spacing with measure units
-grep -HiRns '\bg\b' _posts/*
+grep -HiRns '\bg\b' _posts/
 
-grep -HiRns ' l\b' _posts/*
-grep -HiRns ' ml\b' _posts/*
+grep -HiRns ' l\b' _posts/
+grep -HiRns ' ml\b' _posts/
 
-grep -HiRns '[0-9½¼]EL\b' _posts/*
-grep -HiRns '[0-9½¼]TL\b' _posts/*
+grep -HiRns '[0-9½¼]EL\b' _posts/
+grep -HiRns '[0-9½¼]TL\b' _posts/
 
 # Check for correct spacing at the end of lines
-grep -EHiRns '[^[:space:]][[:space:]]$' _posts/*
-grep -EHiRns '[[:space:]]{3,}$' _posts/*
+grep -EHiRns '[^[:space:]][[:space:]]$' _posts/
+grep -EHiRns '[[:space:]]{3,}$' _posts/
